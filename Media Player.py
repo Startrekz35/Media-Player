735c75ae0795a8ce96c9291d6e76a1441146b43c
Startrekz35 <burkz3905@gmail.com>
2024-10-24 01:26:39 -0500

    Minor Changes

--- Media Player.py
+++ Media Player.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QMenuBar, QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QMenuBar, QAction, QSlider, QVBoxLayout, QWidget
 from PyQt5.QtGui import QKeySequence
 
 
@@ -7,15 +7,24 @@ from PyQt5.QtGui import QKeySequence
 class MediaPlayer(QMainWindow):
     def __init__(self):
         super().__init__()
-
         #Here sets up the title and the size of the window
         self.setWindowTitle('Media Player')
         self.setGeometry(200, 200, 800, 600) #(x, y, width, height)
 
 
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+        main_layout = QVBoxLayout()
+
+        central_widget.setStyleSheet("background-color: black;")
+
+
+        #menu bar list
         menu_bar = self.menuBar()
         file_menu = menu_bar.addMenu("File")
         view_menu = menu_bar.addMenu("View")
+        play_menu = menu_bar.addMenu("Play")
+        
         
         #Opens File + Actions
         open_file = QAction("Open", self)
@@ -23,12 +32,16 @@ class MediaPlayer(QMainWindow):
         open_file.triggered.connect(self.open_video)
 
 
-
-        #Made the File Menu
+        #File Menu List
         file_menu.addAction(open_file)
-        #view_menu.addAction
-
         
+        #Opens View + Actions
+        open_view = QAction("Full Screen", self)
+        open_view.setShortcut(QKeySequence("Ctrl+Enter"))
+
+        #View Menu List
+        view_menu.addAction(open_view)
+
 
     
     
