76983719f3cf24a721a230753337f6389a34ef3b
Startrekz35 <burkz3905@gmail.com>
2024-10-24 00:14:04 -0500

    Adding a Menu Bar

--- Media Player.py
+++ Media Player.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QMenuBar, QAction
+from PyQt5.QtGui import QKeySequence
+
 
 #This sets up the main window class
 class MediaPlayer(QMainWindow):
@@ -8,8 +10,32 @@ class MediaPlayer(QMainWindow):
 
         #Here sets up the title and the size of the window
         self.setWindowTitle('Media Player')
-        self.setGeometry(100, 100, 800, 600) #(x, y, width, height)
+        self.setGeometry(200, 200, 800, 600) #(x, y, width, height)
 
+
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu("File")
+        view_menu = menu_bar.addMenu("View")
+        
+        #Opens File + Actions
+        open_file = QAction("Open", self)
+        open_file.setShortcut(QKeySequence("Ctrl+O"))
+        open_file.triggered.connect(self.open_video)
+
+
+
+        #Made the File Menu
+        file_menu.addAction(open_file)
+        #view_menu.addAction
+
+        
+
+    
+    
+    def open_video(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open Video", "", "Video Files (*.mp4 *.avi *.flv *.mkv *.mov *.avchd *.webm *.avi *.wmv)")
+        if file_name:
+            print(f"Selected video: {file_name}")
 def main():
     app = QApplication(sys.argv) #manages app flow and handles and captures cmd arguments
     window = MediaPlayer()
